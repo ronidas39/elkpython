43e4b1b401993b46a0ade90bd76f538fd78325e7
ronidas39 <ronidas041987@gmail.com>
2023-05-26 20:01:44 +0530

    update

--- Tutorial7/create_doc.py
+++ Tutorial7/create_doc.py
@@ -5,7 +5,8 @@ headers={
     "Content-Type":"application/json",
 }
 json_data={
-    "message":"this is added by python and rest api and id 2"
+    "message":"this is added by python and rest api and id 2",
+     "count":10
 }
-response=requests.put(url+"test_index_using_api/_doc/2?pretty",headers=headers,json=json_data)
+response=requests.put(url+"test_index_using_api/_doc/3?pretty",headers=headers,json=json_data,auth=("elastic", "9R-bViTHVn8xp_wJtDPh"))
 print(response)
